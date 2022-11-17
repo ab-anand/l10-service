05047b22fd935e449d07421491ce2648732713c2
RituDhariwal <ritu@sharechat.co>
2022-11-17 18:25:20 +0530

    lint fix

--- driver/spanner.js
+++ driver/spanner.js
@@ -1,4 +1,4 @@
 const SpannerHelper = require('@db-helper/spanner');
 const spannerHelper = new SpannerHelper('follow-service');
 
-module.exports = { spannerHelper };
+module.exports = { spannerHelper: spannerHelper };
